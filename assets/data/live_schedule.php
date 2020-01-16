ba71bdc4ca9ee6e43858d850de32964b83327476
benank <mp.paradigm@gmail.com>
2020-01-16 11:02:50 -0800

    Update php

--- assets/data/live_schedule.php
+++ assets/data/live_schedule.php
@@ -1,3 +1,4 @@
-header("Access-Control-Allow-Origin: *");
-header("content-type: application/json");
-echo file_get_contents("live_site_schedule.json");
+<?php
+    header("Access-Control-Allow-Origin: *");
+    header("content-type: application/json");
+    echo file_get_contents("live_site_schedule.json");
