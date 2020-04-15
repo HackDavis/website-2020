d0772f913585f30e6bdd7975f3cc87b817b65c7a
benank <terkolskie@gmail.com>
2020-04-15 15:37:03 -0700

    Change url

--- sync-test/client.js
+++ sync-test/client.js
@@ -1,6 +1,6 @@
 $(document).ready(function() 
 {
-    const URL = "https://ac.hackdavis.io:8080"; 
+    const URL = "http://ac.hackdavis.io:8080"; 
     const SOCKET = io.connect(URL, {secure: true});
     let isActive = true; // If this window is currently active
     let target_pos = {x: 0, y: 0};
